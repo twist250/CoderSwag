1219afe38a2fbdb036d858bdaebd7aa15b5d4f12
IRADUKUNDA Olivier <iradukundaolivier08@gmail.com>
2019-10-30 18:19:19 +0200

    added listview on ui

--- app/src/main/java/com/twist/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/twist/coderswag/Controller/MainActivity.kt
@@ -6,8 +6,11 @@ import com.twist.coderswag.R
 
 class MainActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
