72ed56fd30a209b2750be7c867c9d49ba5fb5000
IRADUKUNDA Olivier <iradukundaolivier08@gmail.com>
2019-10-30 19:46:40 +0200

    added recyclerview

--- app/src/main/java/com/twist/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/twist/coderswag/Controller/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.twist.coderswag.Controller
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.twist.coderswag.Adapters.CategoryAdapter
+import com.twist.coderswag.Model.Category
 import com.twist.coderswag.R
 import com.twist.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,5 +18,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+        categoryListView.setOnItemClickListener { parent, view, position, id ->
+            val category: Category = DataService.categories[position]
+            Toast.makeText(this,"You clicked on ${category.title}",Toast.LENGTH_SHORT).show()
+        }
     }
 }
