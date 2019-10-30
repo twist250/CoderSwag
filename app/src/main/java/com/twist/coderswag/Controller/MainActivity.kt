faa3638d19775785268e3db45ea81c080de0f214
IRADUKUNDA Olivier <iradukundaolivier08@gmail.com>
2019-10-30 19:32:28 +0200

    refactored category adapter

--- app/src/main/java/com/twist/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/twist/coderswag/Controller/MainActivity.kt
@@ -1,25 +1,20 @@
 package com.twist.coderswag.Controller
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import com.twist.coderswag.Model.Category
+import com.twist.coderswag.Adapters.CategoryAdapter
 import com.twist.coderswag.R
 import com.twist.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        adapter = ArrayAdapter(
-            this,
-            android.R.layout.simple_list_item_1,
-            DataService.categories
-        )
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
     }
 }
