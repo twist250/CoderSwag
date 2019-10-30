8a94d244eb96803410282e504cb843020e3b0016
IRADUKUNDA Olivier <iradukundaolivier08@gmail.com>
2019-10-30 18:26:47 +0200

    Added categories in listview

--- app/src/main/java/com/twist/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/twist/coderswag/Controller/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.twist.coderswag.Controller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.twist.coderswag.Model.Category
 import com.twist.coderswag.R
+import com.twist.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            DataService.categories
+        )
+        categoryListView.adapter = adapter
     }
 }
