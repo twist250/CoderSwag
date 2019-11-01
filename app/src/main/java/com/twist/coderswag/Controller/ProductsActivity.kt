48acc35c73d1be02f179bd97a2afad264418dcc2
IRADUKUNDA Olivier <iradukundaolivier08@gmail.com>
2019-11-01 21:15:10 +0200

    final commit

--- app/src/main/java/com/twist/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/twist/coderswag/Controller/ProductsActivity.kt
@@ -1,16 +1,35 @@
 package com.twist.coderswag.Controller
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.twist.coderswag.Adapters.ProductsAdapter
 import com.twist.coderswag.R
+import com.twist.coderswag.Services.DataService
 import com.twist.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE)
+            spanCount = 4
+        val screenSize = resources.configuration.screenWidthDp
+        if (screenSize > 720)
+            spanCount = 3
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productsListView.adapter = adapter
+        productsListView.layoutManager = layoutManager
     }
 }
